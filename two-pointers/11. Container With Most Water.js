2c713ea358722fb5307c97a68cb173f99c329848
Volodymyr Lopukhovych <volodymyr.l@btigroup.io>
2025-07-24 23:12:09 -0400

    Feature: Progress day 24 Jul 2025

--- two-pointers/11. Container With Most Water.js
+++ two-pointers/11. Container With Most Water.js
@@ -2,14 +2,15 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-  let left = 0;
-  let right = height.length - 1;
+var maxArea = function (height) {
+  let left = 0, right = height.length - 1;
   let max = 0;
-  while(left < right) {
-    let min = Math.min(height[left], height[right]);
-    max = Math.max(max, min * (right - left));
-    if(height[left] < height[right]) {
+  
+  while (left < right) {
+    let minHeight = Math.min(height[left], height[right]);
+    max = Math.max(max, minHeight * (right - left));
+    
+    if (height[left] < height[right]) {
       left++;
     } else {
       right--;
@@ -19,6 +20,6 @@ var maxArea = function(height) {
 };
 
 // Test
-const height = [1,8,6,2,5,4,8,3,7]; //49
+const height = [1, 8, 6, 2, 5, 4, 8, 3, 7]; //49
 // const height = [1,1]; //1
-
+console.log('maxArea: ', maxArea(height)); // eslint-disable-line no-console
